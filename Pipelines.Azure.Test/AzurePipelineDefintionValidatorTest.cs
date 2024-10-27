f71b7f1e7a069e3cd90eee135a3fb2eb0b46b1a7
byar <byarotsky@gmail.com>
2024-10-27 09:51:25 +0100

    wip - working on unit tests for trigger section validation

--- Pipelines.Azure.Test/AzurePipelineDefintionValidatorTest.cs
+++ Pipelines.Azure.Test/AzurePipelineDefintionValidatorTest.cs
@@ -1,58 +1,91 @@
 namespace Pipelines.Azure.Test;
 
 [TestClass]
-public class AzurePipelineYmlValidatorTest
+public class AzurePipelineYamlValidatorTest
 {
     [TestMethod]
-    public void ValidSimpleYmlProducesNoErrors()
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow("       \n")]
+    public void NoYamlYieldsAnError(string yaml)
     {
-        const string validYml = """
-        trigger:
-        - main
-        
-        pool:
-          vmImage: 'ubuntu-latest'
-        
-        steps:
-        - script: dotnet build
-        """;
-        var errors = FindErrorsWithDefaultDevOpsConfiguration(validYml);
+        var errors = FindErrorsWithDefaultDevOpsConfiguration(yaml);
+        Assert.AreEqual(1, errors.Count);
+        Assert.IsInstanceOfType<AzurePipelineNoYaml>(errors.Single());
+    }
+    
+    [TestMethod]
+    public void YamlWithNoTriggerSectionAndDefaultDevOpsConfigMeansNoErrors()
+    {
+        const string yaml = """
+                            pool:
+                              vmImage: 'ubuntu-latest'
+                            
+                            steps:
+                            - script: dotnet build
+                            """;
+        var errors = FindErrorsWithDefaultDevOpsConfiguration(yaml);
         Assert.AreEqual(0, errors.Count);
     }
-
-    private static List<AzurePipelineYmlError> FindErrorsWithDefaultDevOpsConfiguration(string yaml) 
-        => AzurePipelineYmlValidator.FindErrors(yaml, AzureDevOpsPipelineConfiguration.Default).ToList();
-}
-
-
-public class AzureDevOpsPipelineConfiguration
-{
-    private AzureDevOpsPipelineConfiguration()
+    
+    [TestMethod]
+    public void YamlWithNoTriggerSectionAndDisableImpliedYamlCiTrigger1Error()
     {
+        const string yaml = """
+                                pool:
+                                  vmImage: 'ubuntu-latest'
+
+                                steps:
+                                - script: dotnet build
+                                """;
+        
+        var config = AzureDevOpsPipelineConfiguration.Default with
+        {
+            DisableImpliedYamlCiTrigger = true
+        };
+        
+        var errors = FindErrors(yaml, config);
+        Assert.AreEqual(1, errors.Count);
+        Assert.IsInstanceOfType<AzurePipelineMissingTriggerSection>(errors.Single());
     }
 
-    public static AzureDevOpsPipelineConfiguration Default = new AzureDevOpsPipelineConfiguration();
+    private static List<AzurePipelineYamlError> FindErrorsWithDefaultDevOpsConfiguration(string yaml) 
+        => FindErrors(yaml, AzureDevOpsPipelineConfiguration.Default);
+    
+    private static List<AzurePipelineYamlError> FindErrors(string yaml, AzureDevOpsPipelineConfiguration config)
+        => AzurePipelineYamlValidator.FindErrors(yaml, config).ToList();
 }
 
-public static class AzurePipelineYmlValidator
+
+public record AzureDevOpsPipelineConfiguration
 {
-    public static IEnumerable<AzurePipelineYmlError> FindErrors(string yaml, AzureDevOpsPipelineConfiguration configuration)
+    public bool DisableImpliedYamlCiTrigger { get; init; }
+    private AzureDevOpsPipelineConfiguration() { }
+
+    public static readonly AzureDevOpsPipelineConfiguration Default = new();
+}
+
+public static class AzurePipelineYamlValidator
+{
+    public static IEnumerable<AzurePipelineYamlError> FindErrors(
+        string yaml, AzureDevOpsPipelineConfiguration configuration)
     {
         if (string.IsNullOrWhiteSpace(yaml))
         {
-            yield return AzurePipelineYmlError.NoYaml();
+            yield return new AzurePipelineNoYaml();
             yield break;
         }
         
+        // todo - get first section
         
     }
 
 }
 
-public record AzurePipelineYmlError(string Message)
-{
-    // todo - simple hierarchy?
-    public static AzurePipelineYmlError NoYaml() => new AzurePipelineYmlError("empty");
+public record AzurePipelineYamlError(string Message);
 
+public record AzurePipelineNoYaml() : AzurePipelineYamlError(
+    "There is nothing to validate. Null or empty string was passed to the validator");
 
-};
+public record AzurePipelineMissingTriggerSection() : AzurePipelineYamlError(
+    "If in Azure DevOps it was configured for pipelines to require an explicit trigger defined in yaml...");
